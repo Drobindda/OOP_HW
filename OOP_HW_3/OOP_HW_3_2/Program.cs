8b76de566fec164af4a4b5fb3049a2ba5cb3edc6
Drobindda <drobindda@gmail.com>
2021-10-10 00:07:05 +0300

    HW_3

---
+++ OOP_HW_3/OOP_HW_3_2/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+
+namespace OOP_HW_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   System.Collections.Generic.Dictionary<string, Group> groupList = new System.Collections.Generic.Dictionary<string, Group>();
+
+            while (true)
+            {
+                
+                int opt = Program.Menu();
+
+                if (opt == 1)
+                {
+                    Console.WriteLine("Enter group name: ");
+                    string groupName = Console.ReadLine();
+                    if (groupList.ContainsKey(groupName))
+                    {
+                        Console.WriteLine("Group is already existing");
+                    }
+                    else
+                    {
+                        groupList.Add(groupName, new Group(groupName));
+                        Console.WriteLine("Done");
+                    }
+                }
+                else if (opt == 2)
+                {
+
+                    Console.WriteLine("Enter group name: ");
+                    string groupName = Console.ReadLine();
+                    if (!groupList.ContainsKey(groupName))
+                    {
+                        Console.WriteLine("Group with such name isn't existing");
+                        continue;
+                    }
+                    Console.WriteLine("Enter student name: ");
+                    string studentName = Console.ReadLine();
+                    Console.WriteLine("Is it good or bad student?");
+                    string studentFeature = "";
+                    while (studentFeature != "good" && studentFeature != "bad")
+                    {
+                        Console.WriteLine("Enter \"good\" or \"bad\": ");
+                        studentFeature = Console.ReadLine();
+                    }
+                    
+                    if (studentFeature == "good")
+                        groupList[groupName].AddStudent(new GoodStudent(studentName));
+                    else
+                        groupList[groupName].AddStudent(new BadStudent(studentName));
+                    Console.WriteLine("Done");
+                }
+                else if (opt == 3)
+                {
+
+                    Console.WriteLine("Enter group name: ");
+                    string groupName = Console.ReadLine();
+                    if (!groupList.ContainsKey(groupName))
+                    {
+                        Console.WriteLine("Group with such name isn't existing");
+                        continue;
+                    }
+
+                    Console.WriteLine("Enter an option: ");
+                    Console.WriteLine("1 -- Information");
+                    Console.WriteLine("2 -- Full information");
+                    int optInfo = Convert.ToInt32(Console.ReadLine());
+                    if (optInfo == 1)
+                        groupList[groupName].GetInfo();
+                    else
+                        groupList[groupName].GetFullInfo();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+        static int Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("Enter an option: ");
+            Console.WriteLine("1 -- Add a new group");
+            Console.WriteLine("2 -- Add a new student in an existing group");
+            Console.WriteLine("3 -- Show info about an existing group");
+            Console.WriteLine("other number -- Exit");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    
+    abstract class Student
+    {
+        protected Student(string _name)
+        {
+            name = _name;
+            state = "";
+        }
+        public string name;
+        public string state;
+        public void Relax()
+        {
+            state += "Relax ";
+        }
+        public void Read()
+        {
+            state += "Read ";
+        }
+        public void Write()
+        {
+            state += "Write ";
+        }
+        public abstract void Study();
+
+
+    }
+
+    class GoodStudent : Student
+    {
+        public GoodStudent(string _name) : base(_name)
+        {
+            state += "good ";
+        }
+        public override void Study()
+        {
+            Read();
+            Write();
+            Read();
+            Write();
+            Relax();
+        }
+    }
+
+    class BadStudent : Student
+    {
+        public BadStudent(string _name) : base(_name)
+        {
+            state += "bad ";
+        }
+        public override void Study()
+        {
+            Relax();
+            Relax();
+            Relax();
+            Relax();
+            Read();
+        }
+    }
+
+    class Group
+    {
+        public Group(string _groupName)
+        {
+            groupName = _groupName;
+            students = new System.Collections.Generic.List<Student>();
+        }
+        public string groupName;
+        public System.Collections.Generic.List<Student> students;
+        public void AddStudent(Student st)
+        {
+            students.Add(st);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine(groupName);
+            foreach(Student student in students)
+            {
+                Console.WriteLine(student.name);
+            }
+        }
+        public void GetFullInfo()
+        {
+            Console.WriteLine(groupName);
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.name + " " + student.state);
+            }
+        }
+    }
+}
---
+++ OOP_HW_3/OOP_HW_3_3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace OOP_HW_3_3
+{
+    class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter currency rates: ");
+            Console.WriteLine("USD: ");
+            double USDtoUAH_Rate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("EUR: ");
+            double EURtoUAH_Rate = Convert.ToDouble(Console.ReadLine());
+            Converter converter = new Converter(USDtoUAH_Rate, EURtoUAH_Rate);
+
+            while (true)
+            {
+                int opt = Menu();
+                switch (opt)
+                {
+                    case 1:
+                        converter.ConverterOutput("USD", "UAH");
+                        break;
+                    case 2:
+                        converter.ConverterOutput("EUR", "UAH");
+                        break;
+                    case 3:
+                        converter.ConverterOutput("UAH", "USD");
+                        break;
+                    case 4:
+                        converter.ConverterOutput("UAH", "EUR");
+                        break;
+                    default: return;
+                }
+            }
+        }
+        static int Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("What would you like to convert?");
+            Console.WriteLine("Enter an option: ");
+            Console.WriteLine("1 -- USD to UAH");
+            Console.WriteLine("2 -- EUR to UAH");
+            Console.WriteLine("3 -- UAH to USD");
+            Console.WriteLine("4 -- UAH to EUR");
+            Console.WriteLine("other number -- Exit");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    
+
+    class Converter
+    {
+        public Converter(double usd, double eur)
+        {
+            ExchangeRateUSDtoUAH = usd;
+            ExchangeRateEURtoUAH = eur;
+        }
+        public double ExchangeRateUSDtoUAH = 0;
+        public double ExchangeRateEURtoUAH = 0;
+        public double Convert(string CurrencyFromName, string CurrencyToName, double currencyFromAmount)
+        {
+            if (CurrencyFromName == "USD" && CurrencyToName == "UAH")
+                return USDtoUAH(currencyFromAmount);
+            else if (CurrencyFromName == "EUR" && CurrencyToName == "UAH")
+                return EURtoUAH(currencyFromAmount);
+            else if (CurrencyFromName == "UAH" && CurrencyToName == "USD")
+                return UAHtoUSD(currencyFromAmount);
+            else if (CurrencyFromName == "UAH" && CurrencyToName == "EUR")
+                return UAHtoEUR(currencyFromAmount);
+            else 
+                return double.NaN;
+        }
+        public void ConverterOutput(string CurrencyFromName, string CurrencyToName)
+        {
+            Console.WriteLine("Enter {0} :", CurrencyFromName);
+            double currencyFromAmount = System.Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} {1} will be {2} {3} ", currencyFromAmount, CurrencyFromName, Convert(CurrencyFromName, CurrencyToName, currencyFromAmount), CurrencyToName);
+        }
+        public double UAHtoUSD(double uah)
+        {
+            return uah / ExchangeRateUSDtoUAH;
+        }
+        public double UAHtoEUR(double uah)
+        {
+            return uah / ExchangeRateEURtoUAH;
+        }
+        public double USDtoUAH(double usd)
+        {
+            return usd * ExchangeRateEURtoUAH;
+        }
+        public double EURtoUAH(double eur)
+        {
+            return eur * ExchangeRateEURtoUAH;
+        }
+    }
+}
